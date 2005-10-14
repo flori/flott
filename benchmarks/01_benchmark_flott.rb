f4db8c0f62683e26435c3180b8a63f8813d989c6
flori <flori@f11af0ed-4408-0410-895c-f1525b0d0c21>
2005-10-14 14:16:08 +0000

    *** empty log message ***
    git-svn-id: svn://gate.dyna.ping.de/flott/trunk@808 f11af0ed-4408-0410-895c-f1525b0d0c21

--- benchmarks/01_benchmark_flott.rb
+++ benchmarks/01_benchmark_flott.rb
@@ -7,22 +7,38 @@ require 'erb'
 class BC_FlottTime < Bullshit::TimeCase
   include Flott
 
+  begin
+    require 'kashmir'
+  rescue LoadError
+  end
+  
   def setup
     @time   = 10
-    @null   = File.new('/dev/null', 'w')
-    STDOUT.reopen @null
+    #@null   = File.new('/dev/null', 'w')
+    require 'stringio'
+    @null    = StringIO.new
+    #STDOUT.reopen @null
     @flott  = Parser.new( %'AAAAA[!Time.now]AAAAA' * 10).compile
     @erb    = ERB.new(    %'AAAAA<%=Time.now%>AAAAA' * 10, 0, '%<>')
     @env    = Environment.new(@null)
+    if defined? Kashmir
+      @kashmir = Kashmir.new(%'AAAAA^(Time.now)AAAAA' * 10)
+    end
   end
 
   def benchmark_flott
     @flott.evaluate(@env)
   end
 
-  def benchmark_erb
-    @erb.run
+#  def benchmark_erb
+#    @erb.run
+#  end
+
+if defined? Kashmir
+  def benchmark_kashmir
+    @kashmir.expand(Object.new)
   end
+end
 end
 
 =begin
